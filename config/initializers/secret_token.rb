cc284db19a5b93e371d251b82b800818b67f73d0
Takamasa Suzuki <webcrafts098@gmail.com>
2014-01-27 19:48:05 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nahayoru::Application.config.secret_key_base = '5e55bc4f7180bd1476264ada6d0c30e5a20d26684fc49e1c6f1acb5467cbcf737faeea4b480e847b4abfee9e3038018826d4fb0dd9ead375ab17a3b6d9cc253e'
